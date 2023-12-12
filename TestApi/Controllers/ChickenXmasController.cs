230231151af9e67cb8d5f8a7bd4d15c79d58f0a8
Benjlr <roberts.benjamin.luke@gmail.com>
2023-12-12 15:47:30 +1100

    wip

---
+++ TestApi/Controllers/ChickenXmasController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace TestApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ChickenXmasController : ControllerBase {
+    private static byte[]? _chickenPicture;
+
+    private readonly ILogger<ChickenXmasController> _logger;
+
+    public ChickenXmasController(ILogger<ChickenXmasController> logger) {
+        _logger = logger;
+        if (_chickenPicture != null)
+            return; 
+        
+        try {
+            var names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(@"TestApi.Assets.chickenDreams.jpg");
+            if (stream == null)
+                throw new Exception("No chicken image");
+
+            using (Stream reader = stream) {
+                _chickenPicture = reader.ReadAllBytes();
+            }
+
+        } catch {
+            _chickenPicture = null;
+        }
+   
+    }
+
+    [HttpGet]
+    public IActionResult? Get() {
+        if (_chickenPicture == null)
+            return null;
+        return File(_chickenPicture, "image/jpeg");
+    }
+
+
+}
---
+++ TestApi/StreamExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TestApi;
+
+public static class StreamExtensions
+{
+    public static byte[] ReadAllBytes(this Stream instream)
+    {
+        if (instream is MemoryStream)
+            return ((MemoryStream)instream).ToArray();
+
+        using (var memoryStream = new MemoryStream())
+        {
+            instream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}

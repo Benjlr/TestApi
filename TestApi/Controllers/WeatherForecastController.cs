ec8213fbd3c4a4cbdb4c17e046e65960bff7f840
Benjlr <roberts.benjamin.luke@gmail.com>
2023-12-12 15:35:21 +1100

    wip

--- TestApi/Controllers/WeatherForecastController.cs
+++ TestApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace TestApi.Controllers;
 [ApiController]
@@ -24,5 +26,60 @@ public class WeatherForecastController : ControllerBase {
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+    }
+}
+
+
+[ApiController]
+[Route("[controller]")]
+public class ChickenXmasController : ControllerBase {
+    private static byte[]? _chickenPicture;
+
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<ChickenXmasController> _logger;
+
+    public ChickenXmasController(ILogger<ChickenXmasController> logger) {
+        _logger = logger;
+        if (_chickenPicture != null)
+            return; 
+        
+        try {
+            var names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(@"TestApi.Assets.chickenDreams.jpg");
+            if (stream == null)
+                throw new Exception("No chicken image");
+
+            using (Stream reader = stream) {
+                _chickenPicture = reader.ReadAllBytes();
+            }
+
+        } catch {
+            _chickenPicture = null;
+        }
+   
+    }
+
+    [HttpGet]
+    public IActionResult? Get() {
+        if (_chickenPicture == null)
+            return null;
+        return File(_chickenPicture, "image/jpeg");
+    }
+
+
+}
+public static class StreamExtensions {
+    public static byte[] ReadAllBytes(this Stream instream) {
+        if (instream is MemoryStream)
+            return ((MemoryStream)instream).ToArray();
+
+        using (var memoryStream = new MemoryStream()) {
+            instream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
     }
 }
